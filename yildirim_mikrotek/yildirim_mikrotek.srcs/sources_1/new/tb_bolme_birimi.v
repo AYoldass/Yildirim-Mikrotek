273354d8aeab31e296aced6ad9f5dfb6287eae67
ahmettyoldass@outlook.com <111415036+AYoldass@users.noreply.github.com>
2024-03-16 16:59:39 +0300

    Bolme yazildi.
    İki farkli tasarım denendi bu tasarimda sonuç hatali geliyor ona bakilacak(41/9=47579439 geliyor diğer algoritma basarili)

---
+++ yildirim_mikrotek/yildirim_mikrotek.srcs/sources_1/new/tb_bolme_birimi.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 16.03.2024 15:15:35
+// Design Name: 
+// Module Name: tb_bolme_birimi
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_bolme_birimi(
+
+    );
+endmodule
