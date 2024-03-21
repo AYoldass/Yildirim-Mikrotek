76cb8a2d907acffc306cea899635ef0d8ca27b3f
ahmettyoldass@outlook.com <111415036+AYoldass@users.noreply.github.com>
2024-03-21 23:36:29 +0300

    düzenleme.
    genel kod yapisi düzenlendi.

--- yildirim_mikrotek/yildirim_mikrotek.srcs/sources_1/new/aritmetik_mantik_birimi.v
+++ yildirim_mikrotek/yildirim_mikrotek.srcs/sources_1/new/aritmetik_mantik_birimi.v
@@ -3,16 +3,12 @@
 `include "riscv_controller.vh"
 
 module aritmetik_mantik_birimi(
-
- // kontrol_i sinyalleri
-   input  wire [ 3:0] kontrol_i,
-   // veri sinyalleri
+   input  wire [ 3:0] kontrol_i,// kontrol_i sinyalleri
    input  wire [31:0] value1_i,
    input  wire [31:0] value2_i,
    output wire [31:0] result_o
 );
 
-   // Mantik sinyalleri
    wire [31:0] result_xor;
    wire [31:0] result_or;
    wire [31:0] result_and;
@@ -22,7 +18,6 @@ module aritmetik_mantik_birimi(
    wire [31:0] result_slt;
    wire [31:0] result_sltu;
    
-   // Aritmetik Sinyalleri
    wire [32:0] value1_top = (kontrol_i == `ALU_CIKARMA) ? { value1_i,1'b1} : {value1_i,1'b0};
    wire [32:0] value2_top = (kontrol_i == `ALU_CIKARMA) ? {~value2_i,1'b1} : {value2_i,1'b0};
    wire [32:0] result_top;
@@ -48,15 +43,15 @@ module aritmetik_mantik_birimi(
    assign result_sltu = ( (value1_i) < (value2_i)) ? 32'b1 : 32'b0;
    
    assign result_o = (kontrol_i == `ALU_CIKARMA) | (kontrol_i == `ALU_TOPLAMA) ? result_top[32:1] :
-                    (kontrol_i == `ALU_XOR)                                    ? result_xor :
-                    (kontrol_i == `ALU_OR)                                     ? result_or  :
-                    (kontrol_i == `ALU_AND)                                    ? result_and :
-                    (kontrol_i == `ALU_SLL)                                    ? result_sll :
-                    (kontrol_i == `ALU_SRL)                                    ? result_srl :
-                    (kontrol_i == `ALU_SRA)                                    ? result_sra :
-                    (kontrol_i == `ALU_SLT)                                    ? result_slt :
-                    (kontrol_i == `ALU_SLTU)                                   ? result_sltu:
-                    (kontrol_i == `ALU_GECIR)                                  ? value2_i  :
+                     (kontrol_i == `ALU_XOR)                                   ? result_xor :
+                     (kontrol_i == `ALU_OR)                                    ? result_or  :
+                     (kontrol_i == `ALU_AND)                                   ? result_and :
+                     (kontrol_i == `ALU_SLL)                                   ? result_sll :
+                     (kontrol_i == `ALU_SRL)                                   ? result_srl :
+                     (kontrol_i == `ALU_SRA)                                   ? result_sra :
+                     (kontrol_i == `ALU_SLT)                                   ? result_slt :
+                     (kontrol_i == `ALU_SLTU)                                  ? result_sltu:
+                     (kontrol_i == `ALU_GECIR)                                 ? value2_i  :
                                                                                32'bx;
 
 endmodule
