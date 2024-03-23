1d1f93765433a6b583443a3ea14a7d35da050c50
ahmettyoldass@outlook.com <111415036+AYoldass@users.noreply.github.com>
2024-03-23 17:53:23 +0300

    FPU Devam Edildi.
    toplama-cıkarma,hizalama tamamlandi.

---
+++ yildirim_mikrotek/yildirim_mikrotek.srcs/sources_1/new/kayan_nokta_carpma_birimi.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 23.03.2024 17:24:33
+// Design Name: 
+// Module Name: kayan_nokta_carpma_birimi
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module kayan_nokta_carpma_birimi(
+
+    );
+endmodule
